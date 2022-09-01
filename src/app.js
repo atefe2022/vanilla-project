0e72545afb57fca95b3c92477cd0d7f40ec8140e
atefe2022 <atefehkazemi475@gmail.com>
2022-09-01 20:34:10 +0430

    Update the Time

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 function displayweather(response) {
   console.log(response.data);
-  console.log(response.data.wind.speed);
+  console.log();
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
@@ -15,6 +15,33 @@ function displayweather(response) {
 
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = FormatDate(response.data.dt * 1000);
+}
+
+function FormatDate(TimeStamp) {
+  let date = new Date(TimeStamp);
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minute = date.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  let day = date.getDay();
+  let days = [
+    "sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return `${day} ${hour}:${minute}`;
 }
 
 let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
