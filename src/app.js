9ded981c4879244d5b3ad2e4719324af4f2ff072
atefe2022 <atefehkazemi475@gmail.com>
2022-09-02 01:05:20 +0430

    Set Input City

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ function displayweather(response) {
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute =
     ("src",
-    `openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 
   iconElement.setAttribute = ("alt", response.data.weather[0].description);
 }
@@ -51,17 +51,43 @@ function FormatDate(TimeStamp) {
   return `${day} ${hour}:${minute}`;
 }
 
-let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
-let cityname = "Tehran";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apiKey}&units=metric`;
+//let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
+//let cityname = "Tehran";
+//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
-axios.get(apiUrl).then(displayweather);
+//console.log(apiUrl);
+//axios.get(apiUrl).then(displayweather);
 
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-  return days[day];
+function search(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  console.log(cityInputElement.value);
 }
+
+//let form = document.querySelector("#search-form");
+//form.addEventListener("submit", search);
+
+function MainSearch(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayweather);
+}
+
+function Search(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  MainSearch(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", Search);
+
+MainSearch("New York");
+
+//function formatDay(timestamp) {
+// let date = new Date(timestamp * 1000);
+// let day = date.getDay();
+//let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+// return days[day];
+// }
